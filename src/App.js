158a2db1d3b1d2e0595e486d8ebcb84951bf9aa9
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-04-16 11:01:17 +0500

    connect api + styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import Movie from  './components/Movie';
 
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=44346d7d3ab51accb2a7fa7cbd937920&page=1'
+
+// const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=44346d7d3ab51accb2a7fa7cbd937920&query='
+
 function App() {
-  const movies = ['1', '2', '3']
-  return (
-    <div className="App">
-      {movies.map(movie => (
-        <Movie />
-      ))}
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results)
+      })
+  })
+
+  return ( <div className="App">
+      {movies.length > 0 && 
+      movies.map((movie) => <Movie key={movie.id} {...movie} />
+      )}
     </div>
   );
 }
